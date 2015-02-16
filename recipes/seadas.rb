816e19f5adbd33602c4487cb3f52d6b4f3112b68
JC <jay@alaska.edu>
2015-02-16 11:45:06 -0900

    typo.
    Signed-off-by: JC <jay@alaska.edu>

--- recipes/seadas.rb
+++ recipes/seadas.rb
@@ -1,5 +1,6 @@
 
+#basicly, needs to do this:
 # curl -s http://oceandata.sci.gsfc.nasa.gov/ocssw/install_ocssw.py | python /dev/stdin  --avhrr --aqua --terra --osmi --direct-broadcast -i /opt/seadas
 default['seadas']['components'].each do |name, bits|
-	system("curl -s #{bits["url"]} | #{driver} /dev/stdin #{bits["argsi"].join(" ")} -i #{default['seadas']['path']}")
+	system("curl -s #{bits["url"]} | #{driver} /dev/stdin #{bits["args"].join(" ")} -i #{default['seadas']['path']}")
 end
